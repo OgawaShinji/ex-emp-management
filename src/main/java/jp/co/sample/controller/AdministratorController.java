679b676446fbc7e4c12daaeecd85af644c179222
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-06-17 14:14:33 +0900

    add toLogin on AdministratorController

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 @Controller
@@ -16,6 +17,16 @@ public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
 
+	/**
+	 * LoginFormオブジェクトをModelオブジェクトに格納するための処理
+	 * @return
+	 */
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		LoginForm form=new LoginForm();
+		return form;
+	}
+	
 	/**
 	 * 従業員登録時のリクエストパラメータをModelオブジェクトに格納
 	 * 
@@ -52,5 +63,13 @@ public class AdministratorController {
 
 		administratorService.insert(administrator);
 		return "redirect:/";
+	}
+	/**
+	 * ログイン画面へフォワードする
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login.html";
 	}
 }
